c694d9b40b149f4cb66aba86329978631a9257dc
zaahidimraan <zahidimran479833@gmail.com>
2024-07-29 14:12:20 +0500

    Code change to french,german and spanish

--- recipes/DNS/enhancement/train.py
+++ recipes/DNS/enhancement/train.py
@@ -681,8 +681,8 @@ def dataio_prep(hparams):
     #     "russian_speech",
     # ]
     # speech_dirs = [ 'french_fullband' ,'german_fullband','spanish_fullband','russian_fullband','read_fullband','italian_speech' ]
-    # speech_dirs = [ 'french_fullband' ,'german_fullband','spanish_fullband' ]
-    speech_dirs = [ 'read_fullband']
+    speech_dirs = [ 'french_fullband' ,'german_fullband','spanish_fullband' ]
+    # speech_dirs = [ 'read_fullband']
     audio_length = hparams["audio_length"]
 
     train_shard_patterns = []
