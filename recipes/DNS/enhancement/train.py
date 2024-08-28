ec3d1f6a94acb09c5892522f189fbdb38825da30
zaahidimraan <zahidimran479833@gmail.com>
2024-08-28 10:20:56 +0500

    data change to use three lg fr, german, spanish

--- recipes/DNS/enhancement/train.py
+++ recipes/DNS/enhancement/train.py
@@ -682,7 +682,7 @@ def dataio_prep(hparams):
     #     "russian_speech",
     # ]
     # speech_dirs = [ 'french_fullband' ,'german_fullband','spanish_fullband','russian_fullband','read_fullband','italian_speech' ]
-    speech_dirs = [ 'read_fullband' ]#,'german_fullband','spanish_fullband' ]
+    speech_dirs = [ 'french_fullband' ,'german_fullband','spanish_fullband' ]
     audio_length = hparams["audio_length"]
 
     train_shard_patterns = []
